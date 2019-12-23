f2c39e1599ecc22c86159603748f4420d60d3b82
xfhy <xfhy666@qq.com>
2019-12-23 10:34:47 +0800

    策略模式与简单工厂模式结合

--- src/com/xfhy/strategy/strategy/CashContext.java
+++ src/com/xfhy/strategy/strategy/CashContext.java
@@ -4,18 +4,37 @@ package com.xfhy.strategy.strategy;
  * Created by xfhy on 2019/12/23 10:11
  * Description : 策略模式的环境
  * <p>
- * 通过构造方法传入不同的算法实现类,然后即可得到结果
+ * 1. 原始策略模式  : 通过构造方法传入不同的算法实现类,然后即可得到结果.  但是每次都需要在外面创建策略的算法,外界需要了解的就特别多,每次改动也需要在外面改.
+ * 2. 策略+工厂 : 可以配合简单工厂模式,在环境类里面构建算法类,外面无需了解内部任何类,只需和环境类打交道.
  */
 class CashContext {
 
-    private CashSuper mCashSuper;
+    //private CashSuper mCashSuper;
+    private String type;
 
-    public CashContext(CashSuper cashSuper) {
+    /*public CashContext(CashSuper cashSuper) {
         mCashSuper = cashSuper;
+    }*/
+
+    public CashContext(String type) {
+        this.type = type;
     }
 
     public double getResult(double money) {
-        return mCashSuper.acceptCash(money);
+        CashSuper cashSuper;
+        switch (type) {
+            case "正常收费":
+            default:
+                cashSuper = new CashNormal();
+                break;
+            case "满300减100":
+                cashSuper = new CashReturn(300, 100);
+                break;
+            case "打8折":
+                cashSuper = new CashRebate(0.8);
+                break;
+        }
+        return cashSuper.acceptCash(money);
     }
 
 }
--- src/com/xfhy/strategy/strategy/TestStrategy.java
+++ src/com/xfhy/strategy/strategy/TestStrategy.java
@@ -3,17 +3,29 @@ package com.xfhy.strategy.strategy;
 /**
  * Created by xfhy on 2019/12/23 10:08
  * Description :
+ * <p>
+ * 1. 原始策略模式  : 通过构造方法传入不同的算法实现类,然后即可得到结果.  但是每次都需要在外面创建策略的算法,外界需要了解的就特别多,每次改动也需要在外面改.
+ * 2. 策略+工厂 : 可以配合简单工厂模式,在环境类里面构建算法类,外面无需了解内部任何类,只需和环境类打交道.
  */
 class TestStrategy {
 
     public static void main(String[] args) {
-        CashContext cashContext = new CashContext(new CashNormal());
+        /*CashContext cashContext = new CashContext(new CashNormal());
         System.out.println(cashContext.getResult(100));
 
         cashContext = new CashContext(new CashRebate(0.8));
         System.out.println(cashContext.getResult(100));
 
         cashContext = new CashContext(new CashReturn(300, 100));
+        System.out.println(cashContext.getResult(600));*/
+
+        CashContext cashContext = new CashContext("打8折");
+        System.out.println(cashContext.getResult(100));
+
+        cashContext = new CashContext("正常收费");
+        System.out.println(cashContext.getResult(100));
+
+        cashContext = new CashContext("满300减100");
         System.out.println(cashContext.getResult(600));
     }
 
